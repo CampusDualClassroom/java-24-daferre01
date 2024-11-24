6d755d00add8554e67f7efd7e33ed80343a0a105
David Fernandez Reboredo <davidfr2001@hotmail.com>
2024-11-24 20:32:02 +0100

    Ejercicio hecho

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,22 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
-
+        Queue<String> cola = new LinkedList<>();
+        cola.add("Smith");
+        cola.add("Montessori");
+        cola.add("Peralta");
+        cola.add("House");
+        return cola;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
     }
 
     public static void main(String[] args) {
-
+        Queue<String> cola = createQueue();
+        printAndEmptyQueue(cola);
     }
 
 }
